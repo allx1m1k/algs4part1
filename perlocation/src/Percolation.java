200c26a0d34443777fd7d9b9175a41d13018e6bf
allx1m1k <dima@ua2012.org.ua>
2015-01-25 19:55:50 +0200

    Percolation sources

---
+++ perlocation/src/Percolation.java
@@ -0,0 +1,36 @@
+/**
+ * Created by dima on 1/25/2015.
+ */
+public class Percolation {
+    public Percolation(int N)               // create N-by-N grid, with all sites blocked
+    {
+        /* TODO */
+    }
+
+    public void open(int i, int j)          // open site (row i, column j) if it is not open already
+    {
+        /* TODO */
+    }
+    public boolean isOpen(int i, int j)     // is site (row i, column j) open?
+    {
+        /* TODO */
+        return true;
+    }
+    public boolean isFull(int i, int j)     // is site (row i, column j) full?
+    {
+        /* TODO */
+        return true;
+    }
+    public boolean percolates()             // does the system percolate?
+    {
+        /* TODO */
+        return true;
+    }
+
+    public static void main(String[] args)   // test client (optional)
+    {
+        System.out.println("Hello algs4part1");
+
+    }
+
+}
