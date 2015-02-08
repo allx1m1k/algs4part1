59dd5127d0a83c6edc72deb43a08cdd572437e68
allx1m1k <dima@ua2012.org.ua>
2015-02-09 00:36:58 +0200

    Percolation class done

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -1,22 +1,31 @@
 /**
- * Created by dima on 1/25/2015.
- */
+ *  The <tt>Percolation</tt> class represents a answer for 1 programming assignment.
+ *  <p>
+ *  This implementation uses <tt>WeightedQuickUnionUF</> data structure.
+ *  Initializing a data structure with <em>quadratic</em> time.
+ *  Afterwards, <em>union</em>, <em>find</em>, and <em>connected</em> take
+ *  time linear time (in the worst case) and <em>count</em> takes constant
+ *  time.
+ *  <p>
+ *  For specification of assignment, see <a href="http://coursera.cs.princeton.edu/algs4/assignments/percolation.html">Percolation: Instructions</a>
+ *
+ *  @author Dmytro Peltik
+ *
+  */
 public class Percolation {
-    //https://github.com/m14t/coursera.org/blob/master/algs4/002_Percolation/Percolation.java
-    private int rowLen;
-    private int topIndex;
-    private int bottomIndex;
-    private int gridSize;
-    private boolean[] grid;
-    private boolean percolates;
-
-    private WeightedQuickUnionUF uf;
+    private int rowLen; //number of rows
+    private int topIndex; //top index
+    private int bottomIndex; //bottom index
+    private int gridSize; //number of sites
+    private boolean[] grid; //matrix of sites
+    private boolean percolates; //does grid percolates
+    private WeightedQuickUnionUF uf; //union find model
 
     /**
-     *
-     * @param N
+     * Create N-by-N grid, with all sites blocked
+     * @param N the number of sites
      */
-    public Percolation(int N)               // create N-by-N grid, with all sites blocked
+    public Percolation(int N)
     {
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException(
@@ -29,9 +38,13 @@ public class Percolation {
         grid = new boolean[gridSize];
         topIndex = gridSize;
         bottomIndex = gridSize + 1;
-
     }
 
+    /**
+     * Checks if row and column are valid
+     * @param i row number
+     * @param j column nubmer
+     */
     private void checkInput(int i, int j) {
         if (1 > i || rowLen < i) {
             throw new java.lang.IndexOutOfBoundsException(
@@ -45,7 +58,11 @@ public class Percolation {
         }
     }
 
-
+    /**
+     * Opens the particular site
+     * @param iOne row index
+     * @param jOne column index
+     */
     public void open(int iOne, int jOne) {
         checkInput(iOne, jOne);
 
@@ -88,6 +105,12 @@ public class Percolation {
         }
     }
 
+    /**
+     * Translate 2D row and column into int
+     * @param i row number
+     * @param j column number
+     * @return index
+     */
     private int getIndex(int i, int j) {
         return i*rowLen + j;
     }
@@ -135,12 +158,21 @@ public class Percolation {
         return -1;
     }
 
-
+    /**
+     * Checks if the particular index is open (true), or not (false)
+     * @param index site index
+     * @return true is open site
+     */
     private boolean isOpen(int index) {
         return grid[index];
     }
 
-    // is site (row i, column j) open?
+    /**
+     * is site (row i, column j) open?
+     * @param i row number
+     * @param j column number
+     * @return true is open site
+     */
     public boolean isOpen(int i, int j) {
         checkInput(i, j);
 
@@ -148,7 +180,12 @@ public class Percolation {
         return isOpen(getIndex(i-1, j-1));
     }
 
-    // is site (row i, column j) full?
+    /**
+     * is site (row i, column j) full?
+     * @param i row number
+     * @param j column number
+     * @return true is full site
+     */
     public boolean isFull(int i, int j) {
         checkInput(i, j);
 
@@ -156,35 +193,44 @@ public class Percolation {
         return uf.connected(topIndex, getIndex(i-1, j-1));
     }
 
-    // does the system percolate?
+    /**
+     * does the system percolate?
+     * @return true is grid percolates
+     */
     public boolean percolates() {
         if (!percolates) {
             percolates = uf.connected(topIndex, bottomIndex);
         }
         return percolates;
     }
-    public static void main(String[] args)   // test client (optional)
+
+    /**
+     * test client (optional)
+     * @param args class arguments
+     */
+    public static void main(String[] args)
     {
         //System.out.println("Hello algs4part1");
         int N = StdIn.readInt();
         //Percolation pf = new Percolation(3);
         //QuickUnionUF uf = new QuickUnionUF(N);
         Percolation p = new Percolation(N);
-        /*
+
         p.open(1, 1);
         p.open(2, 1);
         p.open(2, 2);
         p.open(3, 2);
         p.open(3, 3);
         p.open(4, 3);
-        */
-        p.open(1,3);
+
+        /*p.open(1,3);
         p.open(2,3);
         p.open(3,3);
         p.open(3,1);
         p.open(2,1);
         p.open(1,1);
         System.out.println(p.percolates);
+        */
     }
 
 }
