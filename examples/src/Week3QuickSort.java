3ef0e8eb415a4cf80ec9b656435d8735fde762f2
allx1m1k <dima@ua2012.org.ua>
2015-03-04 21:01:39 +0200

    Week 3 Quicksort

---
+++ examples/src/Week3QuickSort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+/**
+ * Created by dima on 04.03.2015.
+ */
+public class Week3Quicksort {
+
+    public static void main(String[] args) {
+        int [] values = {71, 64, 34, 76, 54, 73, 87, 83, 29, 70, 61, 50};
+        Arrays.sort(values);
+        System.out.println(Arrays.toString(values));
+
+    }
+}
